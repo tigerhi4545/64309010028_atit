c30808d64ee6330b669dddd41f269d91c21b3cca
tigerhi4545 <64309010028@tnk.ac.th>
2022-11-11 13:21:48 +0700

    Create TestJS.js

---
+++ week4/TestJS.js
@@ -0,0 +1,11 @@
+var myName = 'Atit';
+console.log(myName);
+
+let lastName = 'Jantarachot';
+const age = 20;
+var year = 2022;
+let grade = 3.96;
+grade = Math.floor(grade);
+console.log(grade);
+
+console.log(`My name is${myName}`);
