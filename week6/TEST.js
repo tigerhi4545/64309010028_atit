2a22a4ad6d06db3462c12b98d8601fc372e6d91e
tigerhi4545 <64309010028@tnk.ac.th>
2022-11-25 13:53:55 +0700

    Create TEST.js

---
+++ week6/TEST.js
@@ -0,0 +1,14 @@
++var emp=[{
+    "IDemp":20001,
+    "Name":"นางสาว จันทรรัตน์ ปรีดาศิริกุล",
+    "Dept":"HR ",
+    "Position":"System Engineer",
+    "salary":27000,
+    "Bonus":"A",
+},{
+    "IDemp":20002,
+    "Name":"นายภูมิพัฒน์ วัชรจิระกุล",
+    "Dept":"IT",
+    "Position":"System Engineer",
+    "salary":13000,
+    "Bonus":"A",
